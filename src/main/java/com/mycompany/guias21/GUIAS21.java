f0ef605f7e089a0cd4ed5e177b188ef3555cffae
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 19:32:56 -0500

    primer GUIA21

---
+++ src/main/java/com/mycompany/guias21/GUIAS21.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias21;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS21 {
+
+    public static void main(String[] args) {
+        new pantalla21().setVisible(true);
+    }
+}
